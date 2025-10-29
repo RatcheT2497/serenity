56449afc1ff0047ec1e03320f261a1a06cb99912
RatcheT2497 <ratchetnumbers@proton.me>
2025-10-29 22:15:27 +0100

    LibACPI: Finish renaming all-uppercase enum values

--- Userland/Libraries/LibACPI/NodeData.cpp
+++ Userland/Libraries/LibACPI/NodeData.cpp
@@ -11,13 +11,13 @@ namespace ACPI {
 AK::ErrorOr<i64> NodeData::as_integer()
 {
     switch (m_type) {
-    case NodeData::Type::BYTE:
+    case NodeData::Type::Byte:
         return static_cast<i64>(m_data.get<i8>());
-    case NodeData::Type::WORD:
+    case NodeData::Type::Word:
         return static_cast<i64>(m_data.get<i16>());
-    case NodeData::Type::DWORD:
+    case NodeData::Type::DWord:
         return static_cast<i64>(m_data.get<i32>());
-    case NodeData::Type::QWORD:
+    case NodeData::Type::QWord:
         return static_cast<i64>(m_data.get<i64>());
     default:
         return Error::from_string_literal("Unimplemented cast!");
